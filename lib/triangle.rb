2e9b7718b10054778d0019bd701a431ac424e41f
Katie Blizzard <kblizzard11@gmail.com>
2019-03-25 00:01:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,11 @@ class Triangle
   end 
   
   def kind 
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
+      raise TriangleError
+    elsif 
+    elsif @side1 == @side2 && @side1 == @side3
+      "equilateral"
     
   end 
   
